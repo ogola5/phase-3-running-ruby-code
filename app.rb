631dc3c041f6b0786ca6027a0979c5de4f5776a0
ogola5 <ogolaevance5@gmail.com>
2022-10-19 15:41:41 +0300

    completed assignment

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
-puts [1,2,3]
+puts "Hello World!"
 
-p[1,2,3]
-
-pp[{id:1,hello:"world"},{id:2,hello:"Ruby"},{id:3,hello:"Moon"},{id:4,hello:"Learner"}]
+print "Pass this test, please."
+#puts [1,2,3]
+p [1,2,3]
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
